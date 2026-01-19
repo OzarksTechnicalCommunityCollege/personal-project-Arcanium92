12766c36f30ad8caf1740766b1da9b979c42e117
Nick <nm0960915@otc.edu>
2026-01-19 12:23:25 -0600

    Progress save for activating the application and adding to the apps.py file

--- PersonalProject/PersonalProject/settings.py
+++ PersonalProject/PersonalProject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'game_review.apps.GameReviewConfig', #create database tables for game_review models
 ]
 
 MIDDLEWARE = [
--- PersonalProject/game_review/models.py
+++ PersonalProject/game_review/models.py
@@ -11,5 +11,11 @@ class GameReview(models.Model):
     created = models.DateTimeField(auto_now_add=True) #Stores date and time post was created
     updated = models.DateTimeField(auto_now_add=True) #Stores time and date post was updated
 
+    class Meta:
+        ordering = ['-review_text'] #Meta class for sort order
+        indexes = [
+            models.Index(fields=['-review_text']),
+        ] # Database index
+
     def __str__(self):
         return self.title, self.reviewer, self.rating, self.review_text, self.submission
