1db71add671fc1c7478edaef695099c8078b5ddf
Nick <nm0960915@otc.edu>
2026-01-19 12:11:44 -0600

    Created the models.py file

--- PersonalProject/game_review/models.py
+++ PersonalProject/game_review/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.db.models.functions import Now
 
 # Create your models here.
+class GameReview(models.Model):
+    title = models.CharField(max_length=250)
+    reviewer = models.CharField(max_length=250)
+    rating = models.IntegerField()
+    review_text = models.TextField()
+    submission = models.DateTimeField(db_default=Now())
+    created = models.DateTimeField(auto_now_add=True) #Stores date and time post was created
+    updated = models.DateTimeField(auto_now_add=True) #Stores time and date post was updated
+
+    def __str__(self):
+        return self.title, self.reviewer, self.rating, self.review_text, self.submission
