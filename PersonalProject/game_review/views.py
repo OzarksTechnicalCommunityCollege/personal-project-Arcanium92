d8a99660099612c3f41def82972a3cbd689b7cca
Nick <nm0960915@otc.edu>
2026-01-19 20:37:35 -0600

    Progress save

--- PersonalProject/game_review/views.py
+++ PersonalProject/game_review/views.py
@@ -4,4 +4,4 @@ from .models import GameReview
 # Create your views here.
 def review_list(request):
     reviews = GameReview.objects.all()
-    return render(request, "reviews/review_list.html", {"reviews": reviews})
+    return render(request, "game_review/review_list.html", {"reviews": reviews})
