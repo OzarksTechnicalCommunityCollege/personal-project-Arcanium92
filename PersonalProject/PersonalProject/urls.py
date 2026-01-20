e27fe0c94f69e3461ce1033727a9d54f012067c3
Nick <nm0960915@otc.edu>
2026-01-20 12:28:13 -0600

    Progress save. Had to move files to local machine and re-path to avoid runtime errors.

--- PersonalProject/PersonalProject/urls.py
+++ PersonalProject/PersonalProject/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('reviews/', include('game_review.urls')),
+    path("", lambda request: redirect("add_review"), name="root_redirect"),
+    path("admin/", admin.site.urls),
+    path("reviews/", include("game_review.urls")),
 ]
