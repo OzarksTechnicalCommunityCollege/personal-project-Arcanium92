5af83b9918bf4be9d7f83f93063065acdfb8fb14
Nick <nm0960915@otc.edu>
2026-01-26 19:06:59 -0600

    progress save

--- PersonalProject/game_review/views.py
+++ PersonalProject/game_review/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, review
 from .models import GameReview
 from .forms import GameReviewForm
 
@@ -12,7 +12,8 @@ def add_review(request):
         form = GameReviewForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('home')
+            
+            return render(request, "review/review_result.html", {"review": review})
     else:
         form = GameReviewForm()
     return render(request, "add_review/add_review.html", {"form": form})
