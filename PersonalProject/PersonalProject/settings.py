5c9c1f97818ba917057911f9792ea5f41b9e7ee9
Nick <nm0960915@otc.edu>
2026-02-03 22:22:02 -0600

    updated settings.py for Postgresql, created admin.py, saved data to json

--- PersonalProject/PersonalProject/settings.py
+++ PersonalProject/PersonalProject/settings.py
@@ -9,11 +9,15 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import environ
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
@@ -74,10 +78,16 @@ WSGI_APPLICATION = 'PersonalProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
+# Updated the database from SqlLite to Postgres 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT')
     }
 }
 
--- PersonalProject/game_review/admin.py
+++ PersonalProject/game_review/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
+from .models import GameReview
 
 # Register your models here.
-from .models import GameReview
-admin.site.register(GameReview)
+
+# List view, filters and search bar for columns
+@admin.register(GameReview)
+class GameReviewAdmin(admin.ModelAdmin):
+    list_display = ('title', 'reviewer', 'rating', 'submission', 'created')
+    list_filter = ('rating', 'submission', 'created')
+    search_fields = ('title', 'reviewer', 'review_text')
+    ordering = ('-created',)
+    readonly_fields = ('created', 'updated', 'submission')
