0c37b4a119701d2374d58fd83076616a2877a022
Nick <nm0960915@otc.edu>
2026-03-08 14:22:43 -0500

    fixed spelling in signals.py file

--- PersonalProject/game_review/signals.py
+++ PersonalProject/game_review/signals.py
@@ -3,7 +3,7 @@ from django.dispatch import receiver
 from django.conf import settings
 from .models import Profile
 
-@reciever(post_save, sender=settings.AUTH_USER_MODEL)
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
