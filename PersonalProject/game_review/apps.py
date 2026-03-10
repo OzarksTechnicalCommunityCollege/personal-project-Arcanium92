eceb1d7f85f53183680ce9970956308b529d9904
Nick <nm0960915@otc.edu>
2026-03-10 10:16:33 -0500

    Added signals in the apps.py file

--- PersonalProject/game_review/apps.py
+++ PersonalProject/game_review/apps.py
@@ -1,7 +1,13 @@
 from django.apps import AppConfig
 
-
 class GameReviewConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'game_review'
 
+# Signals
+class GameReviewConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'game_review'
+
+    def ready(self):
+        import game_review.signals
