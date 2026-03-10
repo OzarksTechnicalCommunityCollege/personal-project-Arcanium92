f29151d9e38b433ec5eda3af80d2a1a3ffb45aff
Nick <nm0960915@otc.edu>
2026-03-09 19:21:58 -0500

    Modified the settings.py for Redis cache

--- PersonalProject/PersonalProject/settings.py
+++ PersonalProject/PersonalProject/settings.py
@@ -34,6 +34,13 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379',
+    }
+}
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
