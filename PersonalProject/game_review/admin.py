13348ff702190c5f08e49c28d19772943639f20e
Nick <nm0960915@otc.edu>
2026-03-31 21:52:44 -0500

    Registering models to the admin.py for Game and Genre

--- PersonalProject/game_review/admin.py
+++ PersonalProject/game_review/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import GameReview
+from .models import GameReview, Game, Genre
 
 # Register your models here.
 
@@ -11,3 +11,28 @@ class GameReviewAdmin(admin.ModelAdmin):
     search_fields = ('title', 'reviewer', 'review_text')
     ordering = ('-created',)
     readonly_fields = ('created', 'updated', 'submission')
+
+# Game admin model
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+# Genre admin model    
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'slug',
+        'game',
+        'price',
+        'image_preview',
+    )
+    list_filter = ('game',)
+    search_fields = ('name', 'slug', 'description')
+    prepopulated_fields = {'slug': ('name',)}
+    readonly_fields = ('image_preview',)
+
+    def image_preview(self, obj):
+        if obj.image:
+            return obj.image.url
+        return "No image"
