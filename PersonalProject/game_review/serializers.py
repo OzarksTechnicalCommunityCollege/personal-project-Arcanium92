85be301cdc97e9465d46286975169d0bd2f7ce06
Nick <nm0960915@otc.edu>
2026-04-15 12:11:14 -0500

    Added serializer for API

--- PersonalProject/game_review/serializers.py
+++ PersonalProject/game_review/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import GameReview
 
+# Serializer for API
 class GameReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = GameReview
