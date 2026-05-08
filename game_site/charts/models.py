e414212b30b10993c257de7893109163baca1388
Nick <nm0960915@otc.edu>
2026-05-08 12:12:06 -0500

    Created model in the charts app for game name and rating variables.

--- game_site/charts/models.py
+++ game_site/charts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class ReviewScore(models.Model):
+    gameName = models.CharField(max_length=50)
+    rating = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.gameName}: {self.rating}"
