3784f00db6a3242c5e2250522bb59897b82a55d8
Nick <nm0960915@otc.edu>
2026-05-10 09:29:05 -0500

    Added logging and error handling to the prod.py

--- game_site/game_site/settings/local.py
+++ game_site/game_site/settings/local.py
@@ -1,7 +1,6 @@
 from .base import *
 
-SECRET_KEY = "django-insecure-(-!$az5r7*1xl%4)$8kar=hmv#d#3&fe)4o2^*@m(%+z5_8ct@"
-
+SECRET_KEY = 'django-insecure-(-!$az5r7*1xl%4)$8kar=hmv#d#3&fe)4o2^*@m(%+z5_8ct@'
 DEBUG = False
 
 DATABASES = {
--- game_site/game_site/settings/prod.py
+++ game_site/game_site/settings/prod.py
@@ -24,3 +24,26 @@ CSRF_COOKIE_SECURE = True
 SECURE_HSTS_SECONDS = 31536000
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_HSTS_PRELOAD = True
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "file": {
+            "level": "ERROR",
+            "class": "logging.FileHandler",
+            "filename": BASE_DIR / "errors.log",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["file"],
+            "level": "ERROR",
+            "propagate": True,
+        },
+    },
+}
+
+ADMINS = [
+    ("Nick", "nick@otc.edu"),
+]
