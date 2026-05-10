0f64878fd6fcfe9b22602beb9e5c6c6d8f9b33fd
Nick <nm0960915@otc.edu>
2026-05-10 09:21:55 -0500

    Forgot to make branch before making changes.

--- game_site/game_site/settings/local.py
+++ game_site/game_site/settings/local.py
@@ -2,10 +2,15 @@ from .base import *
 
 SECRET_KEY = "django-insecure-(-!$az5r7*1xl%4)$8kar=hmv#d#3&fe)4o2^*@m(%+z5_8ct@"
 
-DEBUG = True
+DEBUG = False
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB", "game_site"),
+        "USER": os.getenv("POSTGRES_USER", "game_site"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "abc123!"),
+        "HOST": os.getenv("POSTGRES_HOST", "localhost"),
+        "PORT": os.getenv("POSTGRES_PORT", "5432"),
     }
 }
--- game_site/game_site/settings/prod.py
+++ game_site/game_site/settings/prod.py
@@ -1,13 +1,26 @@
 from .base import *
+import os
 
 DEBUG = False
-ADMINS = [
-    ('nickm', 'nick@otc.edu'),
-]
-ALLOWED_HOSTS = ['*']
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB", "game_site"),
+        "USER": os.getenv("POSTGRES_USER", "game_site"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "abc123!"),
+        "HOST": os.getenv("POSTGRES_HOST", "localhost"),
+        "PORT": os.getenv("POSTGRES_PORT", "5432"),
     }
 }
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+MEDIA_ROOT = BASE_DIR / "mediafiles"
+
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
