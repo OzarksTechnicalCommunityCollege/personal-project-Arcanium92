416ce111ee8036ba9eca03e9218090003c703d6c
Nick <nm0960915@otc.edu>
2026-05-10 09:59:08 -0500

    Finishing up module-12

--- game_site/game_site/settings/prod.py
+++ game_site/game_site/settings/prod.py
@@ -3,6 +3,10 @@ import os
 
 DEBUG = False
 
+SECRET_KEY = os.getenv("django-insecure-(-!$az5r7*1xl%4)$8kar=hmv#d#3&fe)4o2^*@m(%+z5_8ct@")
+
+ALLOWED_HOSTS = ["127.0.0.1"]
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
