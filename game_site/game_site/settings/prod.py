2d1399df06ad260ce69350f6d705c3f4d3ee7b19
Nick <nm0960915@otc.edu>
2026-05-10 21:46:35 -0500

    Modified files for project security

--- game_site/game_site/settings/prod.py
+++ game_site/game_site/settings/prod.py
@@ -3,20 +3,7 @@ import os
 
 DEBUG = False
 
-SECRET_KEY = os.getenv("django-insecure-(-!$az5r7*1xl%4)$8kar=hmv#d#3&fe)4o2^*@m(%+z5_8ct@")
-
 ALLOWED_HOSTS = ["127.0.0.1"]
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("POSTGRES_DB", "game_site"),
-        "USER": os.getenv("POSTGRES_USER", "game_site"),
-        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "abc123!"),
-        "HOST": os.getenv("POSTGRES_HOST", "localhost"),
-        "PORT": os.getenv("POSTGRES_PORT", "5432"),
-    }
-}
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_ROOT = BASE_DIR / "mediafiles"
